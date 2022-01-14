c1519579eb8eca101a1f48bbce6ca90f80801ec9
Máté Donka <6316925+m4tty-d@users.noreply.github.com>
2022-01-14 09:03:06 +0100

    POCOK-12 - parse url from email, billingo strategy (#6)

--- src/utils/models/structs.go
+++ src/utils/models/structs.go
@@ -4,3 +4,18 @@ type UploadInvoiceMessage struct {
 	Type string `json:"type"` // "url", "base64"
 	Body string `json:"body"`
 }
+
+type EmailAttachment struct {
+	ContentType string `json:"contentType"`
+	Content_b64 string `json:"content_b64"`
+}
+
+type EmailFrom struct {
+	Address string `json:"address"`
+	Name    string `json:"name"`
+}
+type EmailWebhookBody struct {
+	Attachments []*EmailAttachment `json:"attachments"`
+	Html        string             `json:"html"`
+	From        []*EmailFrom       `json:"from"`
+}
