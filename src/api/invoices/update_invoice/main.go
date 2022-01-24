b610efe3533302ad9b7f8981e69431aa2e152604
Tamas Fabi <fabi.tamas.zoltan@gmail.com>
2022-01-24 16:04:40 +0100

    feat: parse update invoice post data

--- src/api/invoices/update_invoice/main.go
+++ src/api/invoices/update_invoice/main.go
@@ -1,12 +1,10 @@
 package main
 
 import (
-	"bytes"
 	"encoding/base64"
-	"io"
-	"mime"
-	"mime/multipart"
+	"errors"
 	"net/http"
+	"net/url"
 	"os"
 	"pocok/src/db"
 	"pocok/src/utils"
@@ -36,18 +34,21 @@ func (d *dependencies) handler(r events.APIGatewayProxyRequest) (*events.APIGate
 	token := r.QueryStringParameters["token"]
 	claims, err := auth.ParseToken(token)
 	if err != nil {
+		utils.LogError("Token validation failed", err)
 		return utils.MailApiResponse(http.StatusUnauthorized, ""), err
 	}
-	data, err := parseFormBody(r)
+	data, err := parseUrlEncodedFormData(r)
 	if err != nil {
-		return utils.MailApiResponse(http.StatusBadRequest, ""), nil
+		utils.LogError("Form body parse failed", err)
+		return utils.MailApiResponse(http.StatusBadRequest, ""), err
 	}
-	id := data["id"]
+	invoiceId := data["invoiceId"]
 	status := data["status"]
-	if id == "" || (status != models.ACCEPTED && status != models.REJECTED) {
+	if invoiceId == "" || (status != models.ACCEPTED && status != models.REJECTED) {
+		utils.LogError("Invalid update payload", errors.New(""))
 		return utils.MailApiResponse(http.StatusUnprocessableEntity, ""), nil
 	}
-	updateErr := db.UpdateInvoiceStatus(d.dbClient, d.tableName, claims.OrgId, id, status)
+	updateErr := db.UpdateInvoiceStatus(d.dbClient, d.tableName, claims.OrgId, invoiceId, status)
 	if updateErr != nil {
 		utils.LogError("Error updating dynamo db", updateErr)
 		return utils.MailApiResponse(http.StatusInternalServerError, ""), nil
@@ -55,36 +56,59 @@ func (d *dependencies) handler(r events.APIGatewayProxyRequest) (*events.APIGate
 	return utils.MailApiResponse(http.StatusOK, ""), nil
 }
 
-func parseFormBody(r events.APIGatewayProxyRequest) (map[string]string, error) {
+func parseUrlEncodedFormData(r events.APIGatewayProxyRequest) (map[string]string, error) {
 	data, err := base64.StdEncoding.DecodeString(r.Body)
 	result := map[string]string{}
 	if err != nil {
 		utils.LogError("Error decoding base64 message content", err)
 		return result, err
 	}
-	reader := bytes.NewReader(data)
-	contentType := r.Headers["content-type"]
-	_, params, err := mime.ParseMediaType(contentType)
-	if err != nil {
-		utils.LogError("Error during parse form media type", err)
-		return result, err
+
+	parsedFormData, parseErr := url.ParseQuery(string(data))
+	if parseErr != nil {
+		utils.LogError("Error parsing url encoded form data", parseErr)
+		return result, parseErr
 	}
-	mr := multipart.NewReader(reader, params["boundary"])
-	for {
-		part, err := mr.NextPart()
-		if err == io.EOF {
-			break
-		}
-		if err != nil {
-			return result, err
-		}
-		slurp, err := io.ReadAll(part)
-		if err != nil {
-			return result, err
-		}
-		key := part.FormName()
-		value := string(slurp)
-		result[key] = value
+
+	for key, value := range parsedFormData {
+		result[key] = value[0]
 	}
+
 	return result, nil
 }
+
+// Turns out that we do not need this...
+//
+// func parseMultipartFormData(r events.APIGatewayProxyRequest) (map[string]string, error) {
+// 	data, err := base64.StdEncoding.DecodeString(r.Body)
+// 	result := map[string]string{}
+// 	if err != nil {
+// 		utils.LogError("Error decoding base64 message content", err)
+// 		return result, err
+// 	}
+// 	reader := bytes.NewReader(data)
+// 	contentType := r.Headers["content-type"]
+// 	_, params, err := mime.ParseMediaType(contentType)
+// 	if err != nil {
+// 		utils.LogError("Error during parse form media type", err)
+// 		return result, err
+// 	}
+// 	mr := multipart.NewReader(reader, params["boundary"])
+// 	for {
+// 		part, err := mr.NextPart()
+// 		if err == io.EOF {
+// 			break
+// 		}
+// 		if err != nil {
+// 			return result, err
+// 		}
+// 		slurp, err := io.ReadAll(part)
+// 		if err != nil {
+// 			return result, err
+// 		}
+// 		key := part.FormName()
+// 		value := string(slurp)
+// 		result[key] = value
+// 	}
+// 	return result, nil
+// }
