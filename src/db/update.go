03ddb74a541642c0cc518422363d447086df89f2
Tamas Fabi <fabi.tamas.zoltan@gmail.com>
2022-01-24 13:51:14 +0100

    fix: db update function

--- src/db/update.go
+++ src/db/update.go
@@ -13,13 +13,13 @@ func UpdateInvoiceStatus(client *dynamodb.Client, tableName string, orgId string
 	_, err := client.UpdateItem(context.TODO(), &dynamodb.UpdateItemInput{
 		TableName: &tableName,
 		Key: map[string]types.AttributeValue{
-			"PK": &types.AttributeValueMemberS{Value: models.ORG + "#" + orgId},
-			"SK": &types.AttributeValueMemberS{Value: models.INVOICE + "#" + invoiceId},
+			"pk": &types.AttributeValueMemberS{Value: models.ORG + "#" + orgId},
+			"sk": &types.AttributeValueMemberS{Value: models.INVOICE + "#" + invoiceId},
 		},
-		UpdateExpression: aws.String("set #k1 = :v1 and #k2 = :v2"),
+		UpdateExpression: aws.String("set #k1 = :v1, #k2 = :v2"),
 		ExpressionAttributeNames: map[string]string{
-			"k1": "status",
-			"k2": "lsi1sk",
+			"#k1": "status",
+			"#k2": "lsi1sk",
 		},
 		ExpressionAttributeValues: map[string]types.AttributeValue{
 			":v1": &types.AttributeValueMemberS{Value: status},
