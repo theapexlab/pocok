bb4ddccaf1a2e767a8bdd00c61f16b3e23d5f570
Tamas Fabi <fabi.tamas.zoltan@gmail.com>
2022-01-25 10:40:50 +0100

    feat: request parser utils

--- src/api/invoices/update_invoice/main.go
+++ src/api/invoices/update_invoice/main.go
@@ -1,16 +1,15 @@
 package main
 
 import (
-	"encoding/base64"
 	"errors"
 	"net/http"
-	"net/url"
 	"os"
 	"pocok/src/db"
 	"pocok/src/utils"
 	"pocok/src/utils/auth"
 	"pocok/src/utils/aws_clients"
 	"pocok/src/utils/models"
+	"pocok/src/utils/request_parser"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
@@ -37,7 +36,7 @@ func (d *dependencies) handler(r events.APIGatewayProxyRequest) (*events.APIGate
 		utils.LogError("Token validation failed", err)
 		return utils.MailApiResponse(http.StatusUnauthorized, ""), err
 	}
-	data, err := parseUrlEncodedFormData(r)
+	data, err := request_parser.ParseUrlEncodedFormData(r)
 	if err != nil {
 		utils.LogError("Form body parse failed", err)
 		return utils.MailApiResponse(http.StatusBadRequest, ""), err
@@ -55,60 +54,3 @@ func (d *dependencies) handler(r events.APIGatewayProxyRequest) (*events.APIGate
 	}
 	return utils.MailApiResponse(http.StatusOK, ""), nil
 }
-
-func parseUrlEncodedFormData(r events.APIGatewayProxyRequest) (map[string]string, error) {
-	data, err := base64.StdEncoding.DecodeString(r.Body)
-	result := map[string]string{}
-	if err != nil {
-		utils.LogError("Error decoding base64 message content", err)
-		return result, err
-	}
-
-	parsedFormData, parseErr := url.ParseQuery(string(data))
-	if parseErr != nil {
-		utils.LogError("Error parsing url encoded form data", parseErr)
-		return result, parseErr
-	}
-
-	for key, value := range parsedFormData {
-		result[key] = value[0]
-	}
-
-	return result, nil
-}
-
-// Turns out that we do not need this...
-//
-// func parseMultipartFormData(r events.APIGatewayProxyRequest) (map[string]string, error) {
-// 	data, err := base64.StdEncoding.DecodeString(r.Body)
-// 	result := map[string]string{}
-// 	if err != nil {
-// 		utils.LogError("Error decoding base64 message content", err)
-// 		return result, err
-// 	}
-// 	reader := bytes.NewReader(data)
-// 	contentType := r.Headers["content-type"]
-// 	_, params, err := mime.ParseMediaType(contentType)
-// 	if err != nil {
-// 		utils.LogError("Error during parse form media type", err)
-// 		return result, err
-// 	}
-// 	mr := multipart.NewReader(reader, params["boundary"])
-// 	for {
-// 		part, err := mr.NextPart()
-// 		if err == io.EOF {
-// 			break
-// 		}
-// 		if err != nil {
-// 			return result, err
-// 		}
-// 		slurp, err := io.ReadAll(part)
-// 		if err != nil {
-// 			return result, err
-// 		}
-// 		key := part.FormName()
-// 		value := string(slurp)
-// 		result[key] = value
-// 	}
-// 	return result, nil
-// }
