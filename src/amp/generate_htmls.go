dfe989abe58e5ba886a9d6623a711e42d79c602d
Tamas Fabi <fabi.tamas.zoltan@gmail.com>
2022-02-01 19:19:17 +0100

    fix: uncatched errors

--- src/amp/generate_htmls.go
+++ src/amp/generate_htmls.go
@@ -5,18 +5,22 @@ import (
 	"os"
 	"path"
 	"pocok/src/consumers/email_sender/create_email"
+	"pocok/src/utils"
 	"runtime"
 
 	"github.com/joho/godotenv"
 )
 
 func main() {
-	godotenv.Load(".env.local")
+	loadEnvErr := godotenv.Load(".env.local")
+	if loadEnvErr == nil {
+		utils.LogError("Error loading env", loadEnvErr)
+	}
 	testUrl := os.Getenv("API_URL")
-	testLogoUrl := "https://github.com/theapexlab/pocok/raw/master/assets/pocok-logo.png"
 	if testUrl == "" {
 		testUrl = "https://test.com"
 	}
+	testLogoUrl := "https://github.com/theapexlab/pocok/raw/master/assets/pocok-logo.png"
 
 	email_content, _ := create_email.GetHtmlSummary(testUrl, testLogoUrl)
 	writeFileRelative(email_content, "/emails/summary_email.html")
@@ -27,5 +31,8 @@ func main() {
 func writeFileRelative(content string, filepath string) {
 	_, filename, _, _ := runtime.Caller(0)
 	currentPath := path.Dir(filename)
-	os.WriteFile(currentPath+filepath, []byte(content), 0644)
+	writeErr := os.WriteFile(currentPath+filepath, []byte(content), 0644)
+	if writeErr == nil {
+		utils.LogError("Error writing file", writeErr)
+	}
 }
