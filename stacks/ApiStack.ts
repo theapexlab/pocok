a300a7cfbd2363e5d0f0739b396fb1d30a7017c7
Tamas Fabi <fabi.tamas.zoltan@gmail.com>
2022-02-03 17:16:20 +0100

    fix: remove unused env and permission

--- stacks/ApiStack.ts
+++ stacks/ApiStack.ts
@@ -85,14 +85,9 @@ export class ApiStack extends Stack {
               ...ampSharedEnvs,
               tableName: additionalStackProps?.storageStack.invoiceTable
                 .tableName as string,
-              bucketName: additionalStackProps?.storageStack.invoiceBucket
-                .bucketName as string,
-              typlessToken: process.env.TYPLESS_TOKEN as string,
-              typlessDocType: process.env.TYPLESS_DOC_TYPE as string,
             },
             permissions: [
               additionalStackProps?.storageStack.invoiceTable as Table,
-              additionalStackProps?.storageStack.invoiceBucket as Bucket,
             ],
           },
         },
