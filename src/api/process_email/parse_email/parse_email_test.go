45b75381c15755b1dc2dc1f02dbcb2e0a43ce390
nemeth2300 <97028536+nemeth2300@users.noreply.github.com>
2022-02-03 15:44:50 +0100

    POCOK-68 - handle multiple invoices (#41)
    * feat: handle multiple invoices
    
    * truly unique
    
    * fix: ?
    
    * cleanup
    
    Co-authored-by: Peter Nemeth <nemethp@inf.u-szeged.hu>
    Co-authored-by: Tamas Fabi <fabi.tamas.zoltan@gmail.com>

--- src/api/process_email/parse_email/parse_email_test.go
+++ src/api/process_email/parse_email/parse_email_test.go
@@ -9,16 +9,16 @@ import (
 )
 
 var _ = Describe("ParseEmail", func() {
-	var invoiceMessage *models.UploadInvoiceMessage
+	var invoiceMessages []models.UploadInvoiceMessage
 	var testError error
 
 	When("body is malformed", func() {
 		BeforeEach(func() {
-			invoiceMessage, testError = parse_email.ParseEmail("")
+			invoiceMessages, testError = parse_email.ParseEmail("")
 		})
 
 		It("returns nil", func() {
-			Expect(invoiceMessage).To(BeNil())
+			Expect(invoiceMessages).To(BeNil())
 		})
 
 		It("errors", func() {
@@ -28,7 +28,7 @@ var _ = Describe("ParseEmail", func() {
 
 	When("body doesn't contain pdf attachment", func() {
 		BeforeEach(func() {
-			invoiceMessage, testError = parse_email.ParseEmail(`{
+			invoiceMessages, testError = parse_email.ParseEmail(`{
 				"attachments": [
 					{
 						"contentType": "image/gif",
@@ -42,7 +42,7 @@ var _ = Describe("ParseEmail", func() {
 		})
 
 		It("returns nil", func() {
-			Expect(invoiceMessage).To(BeNil())
+			Expect(invoiceMessages).To(BeNil())
 		})
 
 		It("errors", func() {
@@ -52,7 +52,7 @@ var _ = Describe("ParseEmail", func() {
 
 	When("body does contain a pdf attachment", func() {
 		BeforeEach(func() {
-			invoiceMessage, testError = parse_email.ParseEmail(`{
+			invoiceMessages, testError = parse_email.ParseEmail(`{
 				"attachments": [
 					{
 						"contentType": "application/pdf",
@@ -70,9 +70,48 @@ var _ = Describe("ParseEmail", func() {
 		})
 
 		It("returns invoice", func() {
+			invoiceMessage := invoiceMessages[0]
 			Expect(invoiceMessage.Type).To(Equal("base64"))
 			Expect(invoiceMessage.Body).To(Equal("lkjasdlfkjasdlfkjasldfkjasldkfjasdfkl"))
 			Expect(invoiceMessage.Filename).To(Equal("SZERV-2021-87.pdf"))
+		})
+	})
+
+	When("body contains multiple pdf attachments", func() {
+		BeforeEach(func() {
+			invoiceMessages, testError = parse_email.ParseEmail(`{
+				"attachments": [
+					{
+						"contentType": "application/pdf",
+						"content_b64": "1",
+						"length": "1",
+						"transferEncoding": "base64",
+						"fileName": "BRUH-1.pdf"
+					},
+					{
+						"contentType": "application/pdf",
+						"content_b64": "2",
+						"length": "1",
+						"transferEncoding": "base64",
+						"fileName": "BRUH-2.pdf"
+					}
+				]
+			}`)
+		})
+
+		It("not errors", func() {
+			Expect(testError).To(BeNil())
+		})
+
+		It("returns the invoices correctly", func() {
+			Expect(len(invoiceMessages)).To(Equal(2))
+			Expect(invoiceMessages[0].Type).To(Equal("base64"))
+			Expect(invoiceMessages[0].Body).To(Equal("1"))
+			Expect(invoiceMessages[0].Filename).To(Equal("BRUH-1.pdf"))
+
+			Expect(invoiceMessages[1].Type).To(Equal("base64"))
+			Expect(invoiceMessages[1].Body).To(Equal("2"))
+			Expect(invoiceMessages[1].Filename).To(Equal("BRUH-2.pdf"))
 		})
 	})
 })
